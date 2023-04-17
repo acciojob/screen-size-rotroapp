a33dce2d3f70bdc0bfbffd82895a7c378987b90d
Rajat Sharma <35336213+rotroapp@users.noreply.github.com>
2023-04-17 23:34:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+	let disp = document.querySelector('#wnh');
+
+window.addEventListener("resize", function() {
+	disp.innerHTML = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+})
+
+function func() {
+
+	disp.innerHTML = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+}
